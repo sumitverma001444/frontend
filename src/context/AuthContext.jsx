7148cf6f9dabc68a3249795f83de7836aa0c7421
sumitverma001444 <sumitverma4231@gmail.com>
2026-01-02 15:15:38 +0530

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createContext } from 'react'
 export const authDataContext= createContext()
 function AuthContext({children}) {
-    let serverUrl = import.meta.env.VITE_API_URL || "https://backend-wuvd.onrender.com"
+    let serverUrl = import.meta.env.VITE_API_URL || "https://97681302-6d1c-4dc6-865b-debcf2d726b1-00-3g54n2cekoyne.pike.replit.dev/"
 
     let value = {
        serverUrl
